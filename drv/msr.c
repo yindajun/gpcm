04b3ae8d5b98897681fe553d67cf8af09814d295
eric.yin <eric.vmed@hotmail.com>
2013-01-29 13:17:17 +0800

    xxx

--- drv/msr.c
+++ drv/msr.c
@@ -16,3 +16,25 @@
  * $Revision: 0.10 $
  */
 
+
+#include "msr.h"
+#include <wdm.h>
+
+
+#define MSR_NT_DEV_NAME	L"\\Driver\\RDMSR"
+#define MSR_DOS_DEV_NAME L"\\DosDevice\\RDMSR"
+
+DRIVER_INITIALIZE DriverEntry;
+
+__drv_dispatchType(IRP_MJ_CREATE) 
+DRIVER_DISPATCH msr_create;
+
+__drv_dispatchType(IRP_MJ_CLOSE)
+DRIVER_DISPATCH msr_close;
+
+__drv_dispatchType(IRP_MJ_DEVICE_CONTROL)
+DRIVER_DISPATCH msr_ctrl;
+
+DRIVER_UNLOAD msr_unload;
+
+
