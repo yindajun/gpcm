924c19dc7932503d4226a86cebe52b81d241590d
eric.yin <eric.vmed@hotmail.com>
2013-01-29 12:01:48 +0800

    xxx

--- drv/msr.c
+++ drv/msr.c
@@ -1,1 +1,18 @@
- 
+/*
+ * msr.c
+ *
+ * Routines to read write msr.
+ *
+ * Copyright (c) 2013 Eric.  
+ * All rights reserved.
+ *
+ *
+ * Author:
+ *    eric.vinno@hotmail.com
+ */
+
+/*
+ * $Date: 2013/1/29 $
+ * $Revision: 0.10 $
+ */
+
